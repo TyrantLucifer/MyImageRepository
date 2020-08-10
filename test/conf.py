578fee170dcbef58bac4f2ab2bea723f7c4cfffb
TyrantLucifer <TyrantLucifer@gmail.com>
2020-08-10 15:17:02 +0800

    commit conf.py

---
+++ test/conf.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+SUBSCRIBE_URL = 'https://bvfdbd.com/link/9fg49Pf0fUAcrg6A?mu=2'
+SERVER_JSON_FILE_PATH = './ssr_list.json'
+CONFIG_JSON_FILE_PATH = './config.json'
+LOCAL_ADDRESS = '127.0.0.1'
+SHADOWSOCKR_CLIENT_PATH = './shadowsocksr/shadowsocks/local.py'
+TIMEOUT = 300
+WORKERS = 1
+LOCAL_PORT = 1080
+
+
